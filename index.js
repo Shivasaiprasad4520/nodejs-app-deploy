76eb1bd21108c99139b83cfd8101bd8ccfc772a3
shivasai <shivasai@DESKTOP-E99TED0.localdomain>
2026-05-09 14:39:05 +0530

    updated v1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.port ?? 8080
 
 app.get('/', (req, res) => {
-    return res.json({ msg: 'Hello from the server v2' })
+    return res.json({ msg: 'Hello from the server v1' })
 })
 
 app.listen(PORT, () => {
